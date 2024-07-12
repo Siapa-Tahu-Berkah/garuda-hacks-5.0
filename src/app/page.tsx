fa0edc82446af157a98f107687beaa3aab7c2fc1
Koongh <yapkng@gmail.com>
2024-07-12 15:52:24 +0700

    feat: reduce item amount when checkout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,16 +12,28 @@ import animationData2 from "@/assets/good-animation.json";
 import { initializeApp } from "firebase/app";
 import Shop from "@/components/shop";
 import Cookies from "js-cookie";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "./form/firebaseConfig";
 
 export default function Home() {
   const [progress, setProgress] = React.useState(13);
+  const [userData, setUser] = React.useState<any>();
+
+  const getUser = async () => {
+    const user_id = Cookies.get("id");
+    if (user_id) {
+      const userDoc = doc(db, "users", user_id);
+      const snapShotUser = await getDoc(userDoc);
+      const user = snapShotUser.data();
+      console.log(user);
+      setUser(user || "");
+    }
+  };
 
   React.useEffect(() => {
-    const user_id = Cookies.get("id");
-    console.log("user_id", user_id);
-
-    const timer = setTimeout(() => setProgress(66), 500);
-    return () => clearTimeout(timer);
+    getUser();
+    // const timer = setTimeout(() => setProgress(66), 500);
+    // return () => clearTimeout(timer);
   }, []);
 
   return (
@@ -89,12 +101,12 @@ export default function Home() {
             </div>
             <div className="w-full sm:w-3/5 space-y-3">
               <div className="p-4 border border-gray-200 rounded-lg shadow">
-                <h4 className="text-xl font-semibold mb-2">Current XP</h4>
+                <h4 className="text-xl font-semibold mb-2">Your Point</h4>
                 <div className="w-full">
-                  <Progress value={progress} />
+                  <Progress value={userData?.point || 0} />
                 </div>
               </div>
-              <p className="text-sm text-center sm:text-left text-gray-400">
+              {/* <p className="text-sm text-center sm:text-left text-gray-400">
                 It is very important to know who you are. To make decisions. To
                 show who you are.
               </p>
@@ -110,7 +122,7 @@ export default function Home() {
                   />
                   <p className="text-center text-sm font-medium">Kindness</p>
                 </div>
-              </div>
+              </div> */}
             </div>
           </div>
         </div>
